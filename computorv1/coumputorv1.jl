36b33fed54d01045fa6b41adf866f9f7b2757917
senari <onextthenitromancer@gmail.com>
2024-04-13 19:44:41 +0200

    Parsing progress.
    Tokenization of input into digits, Xs and operators. Some validation for
    operators and the shape of words.

--- computorv1/coumputorv1.jl
+++ computorv1/coumputorv1.jl
@@ -10,7 +10,45 @@
 #                                                                              #
 # **************************************************************************** #
 
+function perror(str::AbstractString)
+	printstyled(stderr, "$str\n", color=:red)
+end
+
+function tokenizeInput(inp::String)
+	# Divide the equation into it's left and righthandside:
+	halfs = split(inp, '=')
+	if (length(halfs) !== 2)
+		return perror("The provided input must have one and only one '='.")
+	elseif (isempty(halfs[1]) || isempty(halfs[2]))
+		return perror("The provided input must have both left and right sides in the equation.")
+	end
+
+	# Cleanup of duped operators and token validation:
+	spaceOutOps = (str) -> split(replace(str, r"(\+|-|\*)" => s" \1 "))
+	halfs = Dict("left" => spaceOutOps(halfs[1]), "right" => spaceOutOps(halfs[2]))
+	println(halfs)
+	for (side, val) in halfs
+		if (val[1] == "*")
+			return perror("'*' symbol found at the start of the $side side of the equation.")
+		end
+		if (occursin(r"^[\+\-\*]$", val[end]))
+			return perror("Mathematical operator found at the end of the $side side of the equation.")
+		end
+	end
+	for (i, tk) in pairs(halfs["left"])
+		if (!occursin(r"^(\d|\d?X(\^\d)?|\+|-|\*)$", tk))
+			return perror("Malformed input: '$tk'.")
+		end
+
+		if (tk == "*" && i > 1 && occursin(r"^(\+|-|\*)$", halfs["left"][i - 1]))
+			return perror("Mathematical operator found before '*'.")
+		end
+	end
+end
+
 println("Given input is:")
 for arg in ARGS
 	println(arg)
+	println("\tInitiating tokenization...")
+	tokenizeInput(arg)
 end
