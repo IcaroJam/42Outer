bd00a99d5c996ef688f6e8a79931991918dc8d09
senari <onextthenitromancer@gmail.com>
2024-04-14 11:04:01 +0200

    Lexer!

--- computorv1/coumputorv1.jl
+++ computorv1/coumputorv1.jl
@@ -10,17 +10,18 @@
 #                                                                              #
 # **************************************************************************** #
 
-function perror(str::AbstractString)
+function perror(str::AbstractString, excode = -1)
 	printstyled(stderr, "$str\n", color=:red)
+	exit(excode)
 end
 
 function tokenizeInput(inp::String)
 	# Divide the equation into it's left and righthandside:
 	halfs = split(inp, '=')
 	if (length(halfs) !== 2)
-		return perror("The provided input must have one and only one '='.")
+		perror("The provided input must have one and only one '='.")
 	elseif (isempty(halfs[1]) || isempty(halfs[2]))
-		return perror("The provided input must have both left and right sides in the equation.")
+		perror("The provided input must have both left and right sides in the equation.")
 	end
 
 	# Cleanup of duped operators and token validation:
@@ -29,24 +30,24 @@ function tokenizeInput(inp::String)
 	println(halfs)
 	for (side, val) in halfs
 		if (val[1] == "*")
-			return perror("'*' operator found at the start of the $side side of the equation.")
+			perror("'*' operator found at the start of the $side side of the equation.")
 		end
 		if (occursin(r"^[\+\-\*]$", val[end]))
-			return perror("Mathematical operator found at the end of the $side side of the equation.")
+			perror("Mathematical operator found at the end of the $side side of the equation.")
 		end
 	end
-	dedupedtkns = Dict("left" => String[], "right" => String[])
+	dedupedtkns = Dict("left" => ["+"], "right" => ["+"])
 	for (side, half) in halfs
 		for tk in half
-			if (!occursin(r"^(\d|\d?X(\^\d)?|\+|-|\*)$", tk))
-				return perror("Malformed input: '$tk'.")
+			if (!occursin(r"^(\d+\.?\d*|(\d+\.?\d*)?X(\^\d+)?|\+|-|\*)$", tk))
+				perror("Malformed input: '$tk'.")
 			end
 
 			if (tk == "*" && occursin(r"^(\+|-|\*)$", dedupedtkns[side][end]))
-				return perror("Mathematical operator found before '*'.")
-			elseif (tk == "+" && !isempty(dedupedtkns[side]) && (dedupedtkns[side][end] == "-" || dedupedtkns[side][end] == "+"))
+				perror("Mathematical operator found before '*'.")
+			elseif (tk == "+" && (dedupedtkns[side][end] == "-" || dedupedtkns[side][end] == "+"))
 				continue
-			elseif(tk == "-" && !isempty(dedupedtkns[side]))
+			elseif(tk == "-")
 				if (dedupedtkns[side][end] == "-")
 					dedupedtkns[side][end] = "+"
 				elseif (dedupedtkns[side][end] == "+")
@@ -60,11 +61,60 @@ function tokenizeInput(inp::String)
 		end
 	end
 	println(dedupedtkns)
+	return dedupedtkns
+end
+
+function lexer(tkns::Dict{String, Vector{String}})
+	retPol = [0., 0., 0.] # Organized as [Order0, Order1, Order2]
+	updatePol = function(num, ord, side)
+		sign = side == "left" ? 1 : -1
+
+		retPol[ord + 1] += sign * num
+	end
+
+	for (side, hlf) in tkns
+		num = 0
+		ord = 0
+		i = 1
+		while i <= length(hlf)
+			if (hlf[i] == "-")
+				println("PREADD: $num, $ord")
+				updatePol(num, ord, side)
+				num = -1
+				ord = 0
+			elseif (hlf[i] == "+")
+				println("PREADD: $num, $ord")
+				updatePol(num, ord, side)
+				num = 1
+				ord = 0
+			elseif (hlf[i] == "*")
+				if (hlf[i + 1] == "-")
+					num *= -1
+					i += 1 # Skip straight to the next number or X
+				end
+			else # If it isn't an operator it has to be a number or X
+				sub = match(r"(\d+\.?\d*)?(?:(X)\^?(\d+)?)?", hlf[i]).captures
+				println("CAPTURAS: $sub")
+				if (!isnothing(sub[1]))
+					num *= parse(Float64, sub[1])
+				end
+				if (!isnothing(sub[3]))
+					ord += parse(Int64, sub[3])
+				elseif(!isnothing(sub[2]))
+					ord += 1
+				end
+			end
+			i += 1
+		end
+		println("PREADD: $num, $ord")
+		updatePol(num, ord, side)
+	end
+	println(retPol)
 end
 
 println("Given input is:")
 for arg in ARGS
 	println(arg)
 	println("\tInitiating tokenization...")
-	tokenizeInput(arg)
+	lexer(tokenizeInput(arg))
 end
